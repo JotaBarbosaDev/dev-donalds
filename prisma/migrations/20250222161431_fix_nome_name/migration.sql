597044a2dc2869bf96e734c2b397abd656407a5e
JotaBarbosaDev <jotambbarbosa@gmail.com>
2025-02-22 16:48:05 +0000

    feat: add db seed script

---
+++ prisma/migrations/20250222161431_fix_nome_name/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `nome` on the `MenuCategory` table. All the data in the column will be lost.
+  - You are about to drop the column `nome` on the `Restaurant` table. All the data in the column will be lost.
+  - Added the required column `name` to the `MenuCategory` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `name` to the `Restaurant` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "MenuCategory" DROP COLUMN "nome",
+ADD COLUMN     "name" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Restaurant" DROP COLUMN "nome",
+ADD COLUMN     "name" TEXT NOT NULL;
